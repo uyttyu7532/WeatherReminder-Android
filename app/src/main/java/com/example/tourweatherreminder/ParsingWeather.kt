3a8fe71029858009f9e614541318c475d1f83e5a
choyerin <uyttyu7532@naver.com>
2020-06-03 23:52:03 +0900

    daily hourly파싱

--- app/src/main/java/com/example/tourweatherreminder/ParsingWeather.kt
+++ app/src/main/java/com/example/tourweatherreminder/ParsingWeather.kt
@@ -9,10 +9,9 @@ import java.net.URL
 var latitude: Double? = 37.497886
 var longitude: Double? = 127.027416
 
-
 fun startJSONTask() {
     val task = MyAsyncTask(context = MainActivity())
-    task.execute(URL("https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&&appid=0278d360e035caa40fc3debf63523512&units=metric&exclude=daily,minutely,current"))
+    task.execute(URL("https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&&appid=0278d360e035caa40fc3debf63523512&units=metric&exclude=minutely,current"))
 }
 
 class MyAsyncTask(context: MainActivity) : AsyncTask<URL, Unit, Unit>() {
@@ -20,15 +19,10 @@ class MyAsyncTask(context: MainActivity) : AsyncTask<URL, Unit, Unit>() {
     override fun doInBackground(vararg params: URL?) {
 
         val doc = Jsoup.connect(params[0].toString()).ignoreContentType(true).get()
-        Log.i("파싱1", doc.text())
         val json = JSONObject(doc.text())
-        val hourlyWeather = json.getJSONArray("hourly")
-        Log.i("파싱2", hourlyWeather.toString())
 
-//        Log.i("파싱3",hourlyWeather[0].toString())
-//        Log.i("파싱3",hourlyWeather.getString(1))
-//        Log.i("파싱3",hourlyWeather.getString(2))
-//
+
+        val hourlyWeather = json.getJSONArray("hourly")
         for(i in 0 until hourlyWeather.length()){
             Log.i("파싱dt",hourlyWeather.getJSONObject(i).getString("dt"))
             Log.i("파싱temp",hourlyWeather.getJSONObject(i).getString("temp"))
@@ -39,7 +33,24 @@ class MyAsyncTask(context: MainActivity) : AsyncTask<URL, Unit, Unit>() {
             }
         }
 
-//        Log.i("파싱3",hourlyWeather.names().toString())
+        val dailyWeather = json.getJSONArray("daily")
+        Log.i("파싱daily",dailyWeather.toString())
+
+        for(i in 0 until dailyWeather.length()) {
+            Log.i("파싱dt",dailyWeather.getJSONObject(i).getString("dt"))
+            Log.i("파싱temp",dailyWeather.getJSONObject(i).getJSONObject("temp").getString("day"))
+            Log.i("파싱icon",dailyWeather.getJSONObject(i).getJSONArray("weather").getJSONObject(0).getString("icon"))
+            var tmp = dailyWeather.getJSONObject(i)
+            if (!tmp.isNull("rain")) {
+                Log.i("파싱dailyrain", dailyWeather.getJSONObject(i).getString("rain"))
+            }
+            else{
+                Log.i("파싱dailyrain","0")
+            }
+        }
+
+
+
 
 
 
